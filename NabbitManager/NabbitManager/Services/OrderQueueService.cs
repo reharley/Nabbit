f20493415d2928aae63be9128974347a9030a8fd
manny <mannyharley20@gmail.com>
2019-11-18 07:39:19 -0800

    Fixed DeleteOrderQueue link on release

--- NabbitManager/NabbitManager/Services/OrderQueueService.cs
+++ NabbitManager/NabbitManager/Services/OrderQueueService.cs
@@ -14,7 +14,7 @@ namespace NabbitManager.Services {
 		private const string deleteQueueOrdersUrl = "https://nabbitdev.azurewebsites.net/api/DeleteQueueOrder/restaurantId/{restaurantId}/orderId/{orderId}/orderNumber/{orderNumber}?code=IEy1H8aKmk4qhj1LZma9kPzSJWSUC/72iBOqRoFjZcWkDsm0UM1Veg==";
 #else
 		private const string getQueueOrdersUrl = "https://nabbit.azurewebsites.net/api/GetQueueOrders/restaurantId/{restaurantId}/allOrders/{allOrders}?code=kXf6aapwz4ScevooekM/4H5INYPzaLsr54IXOFeyG6Xm6SxBR2p9VQ==";
-		private const string deleteQueueOrdersUrl = "https://nabbit.azurewebsites.net/api/DeleteQueueOrder/restaurantId/{restaurantId}/orderId/{orderId}?code=UG1uLRRpacxZnSsBdoMoREMNvX2nDq2rMX1qVxbXHpW6LfgsxKPMSg==";
+		private const string deleteQueueOrdersUrl = "https://nabbit.azurewebsites.net/api/DeleteQueueOrder/restaurantId/{restaurantId}/orderId/{orderId}/orderNumber/{orderNumber}?code=UG1uLRRpacxZnSsBdoMoREMNvX2nDq2rMX1qVxbXHpW6LfgsxKPMSg==";
 #endif
 
 		public static int OrderNumber = 0;
