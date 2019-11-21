364c829746cafc1f4a4b22c7a2020621b307bd71
manny <mannyharley20@gmail.com>
2019-11-21 13:10:21 -0800

    added auth files

---
+++ Nabbit/Nabbit/Services/LogOn/B2CConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace Nabbit.Services.LogOn {
+	public static class B2CConstants {
+		// Azure AD B2C Coordinates
+		public static string Tenant = "nabbit.onmicrosoft.com";
+		public static string AzureADB2CHostname = "nabbit.b2clogin.com";
+
+#if __IOS__
+		public static string ClientID = "2297da99-c2fd-4ead-abfb-930c804e2b79";
+#else
+		public static string ClientID = "12023ec5-4650-478f-ae75-0421da37d23a";
+#endif
+
+		public static string PolicySignUpSignIn = "b2c_1_signUpSignIn";
+		public static string PolicyEditProfile = "B2C_1_profileEditing";
+		public static string PolicyResetPassword = "B2C_1_passwordReset";
+
+		public static string[] Scopes = { "https://nabbit.onmicrosoft.com/api/user_impersonation" };
+
+		public static string AuthorityBase = $"https://{AzureADB2CHostname}/tfp/{Tenant}/";
+		public static string AuthoritySignInSignUp = $"{AuthorityBase}{PolicySignUpSignIn}";
+		public static string AuthorityEditProfile = $"{AuthorityBase}{PolicyEditProfile}";
+		public static string AuthorityPasswordReset = $"{AuthorityBase}{PolicyResetPassword}";
+		public static string IOSKeyChainGroup = "com.microsoft.adalcache";
+	}
+}
---
+++ Nabbit/Nabbit/Services/LogOn/UserContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Nabbit.Services.LogOn {
+	public class UserContext {
+		public string Name { get; internal set; }
+		public string UserIdentifier { get; internal set; }
+		public bool IsLoggedOn { get; internal set; }
+		public string GivenName { get; internal set; }
+		public string FamilyName { get; internal set; }
+		public string EmailAddress { get; internal set; }
+		public string AccessToken { get; internal set; }
+	}
+}
